6a1b8da87ca110fa88cd42cef28fc8c38455e713
unknown <jmartinezmateos@iesesteveterradas.cat>
2018-12-11 00:36:57 +0100

    insert datos formulario

--- prueba2.php
+++ prueba2.php
@@ -5,63 +5,69 @@
 </head>
 <body>
 	<?php 
-	//he tenido que hacer en el phpmyadmin
-	/*
-	INSERT INTO `users`(`id_user`, `user`, `password`, `rol`, `name`, `last_name`, `mail`, `phone_number`) VALUES (11,'jmartinez','1234','Developer','Jordi','martinez','ssa@a',123)
-	INSERT INTO `proj_users`(`id_proj_user`, `user`, `cod_project`, `name_proj`) VALUES (5,'jmartinez','30','qui2')
-	INSERT INTO `project`(`id_project`, `cod_project`, `name_project`, `description`, `product_owner`, `scrum_master`, `date_start`, `date_finish`, `comments`, `budget`) VALUES (3,30,'nombre_proy','descripcion','emieza','lzabala',now(),now(),'xcomentario',1000)
-	*/
 
-		$nombre_proyecto = $_POST["nombre"];
-		$campo_scrum_master = $_POST["campo_scrum_master"];
-		$campo_product_owner = $_POST["campo_product_owner"];
-		$numero_proyecto = $_POST["numero_proyecto"];
+		if (isset($_POST["nombre"])){ 
 
+			$nombre_proyecto = $_POST["nombre"];
+			$campo_scrum_master = $_POST["campo_scrum_master"];
+			$campo_product_owner = $_POST["campo_product_owner"];
+			$numero_proyecto = $_POST["numero_proyecto"];
+
+			echo $nombre_proyecto;
+			echo "<br>";
+			echo $campo_scrum_master;
+			echo "<br>";
+			echo $campo_product_owner;
+			echo "<br>";
+			echo $numero_proyecto;
+			echo "<br>";
+
+			$radio = $_POST["checkbox"];
+	                foreach ($radio as $key => $value) {
+	                    echo "<br>$value";
+	                }
+	                
+		 ?>
+
+		<?php
+			
+		   	try {
+		   		$pdo = new PDO("mysql:host=localhost;dbname=scrum2","root","");
+		   		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				$sentencia = $pdo->prepare("INSERT INTO proj_users (id_proj_username, username,cod_project, name_proj) VALUES (:id_proj_username, :username, :cod_project, :name_proj)");
+				$sentencia->bindParam(':id_proj_username', $id_incremental);
+				$sentencia->bindParam(':username',$campo_scrum_master);
+				$sentencia->bindParam(':cod_project', $numero_proyecto);
+				$sentencia->bindParam(':name_proj', $nombre_proyecto);
+
+				$id_incremental = "LAST_INSERT_ID()";
+				$sentencia->execute();
+
+				$sentencia = $pdo->prepare("INSERT INTO project VALUES (:id_project, :cod_project, :name_project, :description, :product_owner, :scrum_master, :date_start, :date_finish, :comments, :budget)");
+				$sentencia->bindParam(':id_project', $id_incremental);
+				$sentencia->bindParam(':cod_project',$numero_proyecto);
+				$sentencia->bindParam(':name_project',$nombre_proyecto);
+				$sentencia->bindParam(':description', $vacio);
+				$sentencia->bindParam(':product_owner', $campo_product_owner);
+				$sentencia->bindParam(':scrum_master', $campo_scrum_master);
+				$sentencia->bindParam(':date_start', $vacio);
+				$sentencia->bindParam(':date_finish', $vacio);
+				$sentencia->bindParam(':comments', $vacio);
+				$sentencia->bindParam(':budget', $vacio);
+
+				$id_incremental = "LAST_INSERT_ID()";
+				$vacio = "";
+				$sentencia->execute();
+			}
+			 catch (PDOException $e) {
+			    echo 'Falló la conexión: ' . $e->getMessage();
+			}
+		}
+		else{
+			echo "no has introducido datos";
+		}
 		
-		echo $nombre_proyecto;
-		echo "<br>";
-		echo $campo_scrum_master;
-		echo "<br>";
-		echo $campo_product_owner;
-		echo "<br>";
-		echo $numero_proyecto;
-		echo "<br>";
-
-		$radio = $_POST["checkbox"];
-                foreach ($radio as $key => $value) {
-                    echo "<br>$value";
-                }
-	 ?>
-
-	<?php
-		$sentencia = $mbd->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
-		$sentencia->bindParam(':name', $nombre);
-		$sentencia->bindParam(':value', $valor);
-
-		// insertar una fila
-		$nombre = 'uno';
-		$valor = 1;
-		$sentencia->execute();
-
-		// insertar otra fila con diferentes valores
-		$nombre = 'dos';
-		$valor = 2;
-		$sentencia->execute();
-
-
-		$sentencia = $mbd->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
-		$sentencia->bindParam(':name', $nombre);
-		$sentencia->bindParam(':value', $valor);
-
-		// insertar una fila
-		$nombre = 'uno';
-		$valor = 1;
-		$sentencia->execute();
-
-		// insertar otra fila con diferentes valores
-		$nombre = 'dos';
-		$valor = 2;
-		$sentencia->execute();
 	?>
 
 </body>
