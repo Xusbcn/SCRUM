a85c794aa744dcd0780e64f4da906dc6036a6c59
unknown <jmartinezmateos@iesesteveterradas.cat>
2018-12-11 00:47:53 +0100

    cambio de acceso pdo

--- home.php
+++ home.php
@@ -66,9 +66,9 @@ echo "<p style='display:none' id='roles'>" . $rol. "</p>";
 <?php 
 	   		$pdo = new PDO("mysql:host=localhost;dbname=scrum2","xus","xus123");		 
 			// Prepare
-			$consulta = $pdo->prepare("SELECT name FROM users where rol = 'ScrumMaster'");
-			$consulta2 = $pdo->prepare("SELECT name FROM users where rol = 'ProductOwner'");
-			$consulta3 = $pdo->prepare("SELECT name FROM users where rol = 'Developer'");
+			$consulta = $pdo->prepare("SELECT username FROM users where rol = 'ScrumMaster'");
+			$consulta2 = $pdo->prepare("SELECT username FROM users where rol = 'ProductOwner'");
+			$consulta3 = $pdo->prepare("SELECT username FROM users where rol = 'Developer'");
 			// Excecute
 			$consulta->execute();
 			$consulta2->execute();
@@ -122,8 +122,8 @@ echo "<p style='display:none' id='roles'>" . $rol. "</p>";
 			while ($respuesta) {
 				?>
 				var opcion_combobox_scrum = document.createElement("option");
-				opcion_combobox_scrum.setAttribute("value",'<?php echo "$respuesta[name]" ?>');
-				var texto_opcion = document.createTextNode('<?php echo "$respuesta[name]" ?>');
+				opcion_combobox_scrum.setAttribute("value",'<?php echo "$respuesta[username]" ?>');
+				var texto_opcion = document.createTextNode('<?php echo "$respuesta[username]" ?>');
 				opcion_combobox_scrum.appendChild(texto_opcion);
 				select_combobox_scrum.appendChild(opcion_combobox_scrum);
 				<?php
@@ -147,8 +147,8 @@ echo "<p style='display:none' id='roles'>" . $rol. "</p>";
 			while ($respuesta2) {
 				?>
 				var opcion_combobox_product = document.createElement("option");
-				opcion_combobox_product.setAttribute("value",'<?php echo "$respuesta2[name]" ?>');
-				var texto_opcion = document.createTextNode('<?php echo "$respuesta2[name]" ?>');
+				opcion_combobox_product.setAttribute("value",'<?php echo "$respuesta2[username]" ?>');
+				var texto_opcion = document.createTextNode('<?php echo "$respuesta2[username]" ?>');
 				opcion_combobox_product.appendChild(texto_opcion);
 				select_combobox_product.appendChild(opcion_combobox_product);
 				<?php
@@ -166,11 +166,11 @@ echo "<p style='display:none' id='roles'>" . $rol. "</p>";
 				var checkbox = document.createElement("input");
 				checkbox.setAttribute("type","checkbox");
 				checkbox.setAttribute("class","checkboxes");
-				checkbox.setAttribute("value",'<?php echo "$respuesta3[name]" ?>');
+				checkbox.setAttribute("value",'<?php echo "$respuesta3[username]" ?>');
 				checkbox.setAttribute("name",'checkbox[]');
 				document.getElementById("formulario_derecha").appendChild(checkbox);
 				var checkbox_label = document.createElement("label");
-				var texto_label = document.createTextNode('<?php echo "$respuesta3[name]" ?>')
+				var texto_label = document.createTextNode('<?php echo "$respuesta3[username]" ?>')
 				checkbox_label.appendChild(texto_label);
 				document.getElementById("formulario_derecha").appendChild(checkbox_label);
 				document.getElementById("formulario_derecha").appendChild(document.createElement("br"));
