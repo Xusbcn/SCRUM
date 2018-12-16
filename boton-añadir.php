93bcf994f94a2394f4a15eb2c9a336ee79d0dfae
marcosarteaga <marcos118118@gmail.com>
2018-12-17 00:11:45 +0100

    añadir boton

---
+++ boton-añadir.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilosañadirboton.css">
+	<script type="text/javascript"  src="botonmas.js"></script>
+	<title></title>
+</head>
+<body onload="inputsNombreProyecto()" >
+
+	<?php  
+		echo "<h2>Espeficicaciones</h2>";
+
+
+
+
+
+
+	?>	
+
+</body>
+</html>
---
+++ botonmas.js
@@ -0,0 +1,45 @@
+function inputsNombreProyecto(){
+	var elementopadre = document.getElementsByTagName('h2')[0];
+	var inputNombre = document.createElement("input");
+	var inputBoton	= document.createElement("input");
+	
+
+	inputBoton.setAttribute("type", "image");
+	inputBoton.setAttribute("src", "img/añadir.png");
+	inputBoton.setAttribute("width", "25");
+	inputBoton.setAttribute("height", "25");
+	
+	inputBoton.addEventListener("click",añadirEspecificacion);
+	inputNombre.setAttribute("name","nombreProyecto");
+
+	insertarDespuesde(elementopadre,inputNombre);
+	insertarDespuesde(inputNombre,inputBoton);
+	
+
+
+	
+}
+
+
+
+
+function añadirEspecificacion(){
+	var elementopadre = document.getElementsByTagName('h2')[0];
+	var creardivnuevo = document.createElement('div');
+	var inputnombreproyecto = document.getElementsByTagName("input")[0].value;
+	var añadirtextoespecificacion = document.createTextNode(inputnombreproyecto);
+	creardivnuevo.appendChild(añadirtextoespecificacion);
+	insertarDespuesde(elementopadre,creardivnuevo);
+
+
+}
+
+
+function insertarDespuesde(e,i){
+	if(e.nextSibling){
+		e.parentNode.insertBefore(i,e.nextSibling);
+
+	} else {
+		e.parentNode.appendChild(i);
+	}
+}
